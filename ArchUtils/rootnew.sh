096900f4c1473bda4e31e76ac2e638cf2aa3c7f9
FOX-GOD <97807485+FOX-GOD@users.noreply.github.com>
2022-04-20 01:06:07 -0400

    Add files via upload

---
+++ ArchUtils/rootnew.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+[[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "without-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PermitRootLogin" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication" /etc/ssh/sshd_config) = '0' ]] && {
+	echo 'PasswordAuthentication yes' > /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+service ssh restart > /dev/null
+#echo -e "\033[1;31mESTABLECER LA CONTRASEÑA ROOT\033[0m"
+#read -p "   [ S | N ]: " -e -i s exit   
+#[[ "$exit" = "n" || "$exit" = "N" ]] && exit => function_
+
+sleep 1s; passwd && rm rootnew.sh &> /dev/null
